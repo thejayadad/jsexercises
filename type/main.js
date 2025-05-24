300540f9458b7b5079525e1716ccf1e65f2a056e
thejayadad <74875595+thejayadad@users.noreply.github.com>
2025-05-24 07:31:08 -0400

    update

--- type/main.js
+++ type/main.js
@@ -1,13 +1,13 @@
-   const text = "Thanks for watching";
-    const el = document.getElementById("typewriter");
-    let index = 0;
 
-    function type() {
-      if (index < text.length) {
-        el.textContent += text.charAt(index);
+const text = "Peace.............."
+const el = document.getElementById("typewritter")
+let index = 0;
+
+function type(){
+    if(index <text.length){
+        el.textContent += text.charAt(index)
         index++;
-        setTimeout(type, 100);
-      }
+        setTimeout(type, 100)
     }
-
-    type();
+}
+type()
